d0d792848e082acc3c468120e3e4921ccd0ea39b
Hubert Wong <hubertwwong@gmail.com>
2016-02-13 11:46:19 -0800

    aaa update fix.

--- aaaUpdateDotFiles.sh
+++ aaaUpdateDotFiles.sh
@@ -2,11 +2,11 @@
 
 # PATH TO THIS DIR.
 # YOU HAVE TO CHANGE THIS ONCE.
-SCRIPTZ_DIR = $HOME/zzz/debScriptz
+SCRIPTZ_HOME_DIR = zzz/debScriptz
 
 # copy files to the home directory.
 echo "> Copying dot files to home directory"
-cp -rpf $HOME/$SCRIPTZ_DIR/dotFiles $HOME/
+cp -rpf $HOME/$SCRIPTZ_HOME_DIR/dotFiles $HOME/
 
 # init bash rc files.
 echo "> Sourcing .bashrc"
