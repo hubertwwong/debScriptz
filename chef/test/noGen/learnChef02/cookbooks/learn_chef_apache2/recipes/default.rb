6ba31c3f9f90a6b0444d57f54526c4f91bccbd6b
Hubert Wong <hubertwwong@gmail.com>
2016-03-03 23:37:13 -0800

    learn test 02

--- chef/test/noGen/learnChef02/cookbooks/learn_chef_apache2/recipes/default.rb
+++ chef/test/noGen/learnChef02/cookbooks/learn_chef_apache2/recipes/default.rb
@@ -6,3 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+package 'apache2'
+
+service 'apache2' do
+  supports :status => true
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
